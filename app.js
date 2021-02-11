862a869bdee7aa43fcf7f20b52c039a092aafc49
Gabriel Simek <gabrielsimek@outlook.com>
2021-02-10 23:55:41 -0800

    addition calculator

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+
+const inputOne = document.getElementById('input-one');
+
+const inputTwo = document.getElementById('input-one');
+
+const addButton = document.getElementById('compute-button')
+
+let output = document.getElementById('output');
+
+addButton.addEventListener('click', () => {
+    output.textContent = Number(inputOne.value) + Number(inputTwo.value);
+
+    
+});
+
+
+
+
